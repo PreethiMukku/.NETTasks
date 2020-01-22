4144aac65bbbb5ae876a48de256af9a73d0dc5ef
PreethiMukku <41248481+PreethiMukku@users.noreply.github.com>
2020-01-22 12:11:08 +0530

    Update CommentsDocumentation.cs

--- Task5/CommentsDocumentation.cs
+++ Task5/CommentsDocumentation.cs
@@ -1,1 +1,40 @@
+using System;
+
+namespace Task5
+{
+    class Example
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            
+            Console.WriteLine("Reverse of a number = "+ obj.ReverseOfNum(2534));
+        }
+    }
+    /// <summary>
+    /// The class contains a method to reverse a number 
+    /// </summary>
+    class Program
+    {
+        int num = 0;
+        /// <summary>
+        /// This method performs reverse operation and returns the result
+        /// </summary>
+        /// <param name="n">
+        /// This takes an integer value
+        /// </param>
+        /// <returns>
+        /// Returns an integer value
+        /// </returns>
+        public int ReverseOfNum(int n)
+        {
+            while (n > 0)
+            {   
+                num = num * 10 + n % 10;
+                n = n / 10;
+            }
+            return num;
+        }
+    }
+}
 
