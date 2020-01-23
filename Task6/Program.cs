94a2c15107301f6170dcdaeeed113de83d3f22a2
PreethiMukku <41248481+PreethiMukku@users.noreply.github.com>
2020-01-23 18:49:08 +0530

    Add files via upload

---
+++ Task6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task6
+{
+    public class Program
+    {
+       
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public int Divide(int a, int b)
+        {        
+            if (b == 0)
+                throw new DivideByZeroException("Dividing by zero");
+            return a / b;
+
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
