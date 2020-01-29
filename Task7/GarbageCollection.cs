e47f824f0c3d12640894f8ec4da6491bf0dda5a9
PreethiMukku <41248481+PreethiMukku@users.noreply.github.com>
2020-01-29 14:19:40 +0530

    Update GarbageCollection.cs

--- Task7/GarbageCollection.cs
+++ Task7/GarbageCollection.cs
@@ -2,12 +2,21 @@ using System;
 
 namespace GarbageCollection
 {
+    /// <summary>
+    /// Class for garbage collector
+    /// </summary>
     class Collector
     {
+        /// <summary>
+        /// constructor of the class
+        /// </summary>
         public Collector()
         {
             Console.WriteLine("Creating Object");
         }
+        /// <summary>
+        /// Destructor
+        /// </summary>
         ~Collector()
         {
             Console.WriteLine("Object is Destroyed");
@@ -21,8 +30,9 @@ namespace GarbageCollection
             Collector obj = new Collector();
             Console.WriteLine("object is created");
             obj = null;
-            Console.WriteLine("Object is assigned to null");
+            // gc.collect() method to destroy unused objects.
             GC.Collect();
+            Console.WriteLine("Object is assigned to null");
             Console.ReadLine();
         }
     }
