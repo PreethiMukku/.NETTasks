101a62cac357ab2bc56816b893fa6a900e03d21a
PreethiMukku <41248481+PreethiMukku@users.noreply.github.com>
2020-01-29 14:18:02 +0530

    Update DisposeExample.cs

--- Task7/DisposeExample.cs
+++ Task7/DisposeExample.cs
@@ -4,33 +4,49 @@ using System.IO;
 
 namespace DisposeExample
 {
+    /// <summary>
+    /// Class implementing Idisposable interface
+    /// </summary>
     class Dispo : IDisposable
     {
         bool disposed = false;
         private Component component = new Component(); // managed resource
         private FileStream file = File.Open(@"C:\Users\Preethi_Mukku\Desktop\hello.txt", FileMode.Open); //unmanaged resource
+
+        /// <summary>
+        /// Dispose method
+        /// </summary>
         public void Dispose()
         {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
 
+        /// <summary>
+        /// overloading Dispose method
+        /// </summary>
+        /// <param name="disposing">Takes boolean value</param>
         private void Dispose(bool disposing)
         {
             if (disposed)
                 return;
             if (disposing)
             {
+                //Code to clean Managed resources 
                 Console.WriteLine("cleared managed resources");
                 component.Dispose();
             }
+            //code to clean Unmanaged resources are cleaned here
             Console.WriteLine("cleared unmanaged resources");
             file.Close();
             disposed = true;
             
         }
 
-        ~Dispo()
+        /// <summary>
+        /// Destructor
+        /// </summary>
+        ~Dispo() //Finalize Method
         {
             Console.WriteLine("Destructor called.");
             Dispose(false);
