be2b3fe524a739b75899dbc0c1e598bff8f5897a
PreethiMukku <41248481+PreethiMukku@users.noreply.github.com>
2020-01-30 14:55:54 +0530

    Update MainProgram.cs

--- Task8/CustomCollections/MainProgram.cs
+++ Task8/CustomCollections/MainProgram.cs
@@ -9,14 +9,15 @@ namespace CustomList
         {
 			ListOperations list = new ListOperations();
 
-			list.Add("Hello");
-			list.Add("Magical");
+			list.Add(4);
+			list.Add(2);
 			list.Add(10);
-			list.Add("World");
+			list.Add(7);
 			list.Add(34);
 			list.Reverse();
 			list.ForEach();
-		
+			list.Sort();
+			list.ForEach();
 		}
     }
 }
