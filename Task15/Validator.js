1cfac1eeb87c4c4d485395c3f5fb2b3a0acc50f4
PreethiMukku <41248481+PreethiMukku@users.noreply.github.com>
2020-02-12 12:54:55 +0530

    Add files via upload

---
+++ Task15/Validator.js
@@ -0,0 +1,44 @@
+﻿function validateForm() {
+  
+    var firstName = document.getElementById("firstname").value;
+    var lastName = document.getElementById("lastname").value;
+    var mobile = document.getElementById("mobile").value;
+    var list = document.getElementsById("GenderList");
+    var gender = list.getElementsByTagName("input");
+    
+    if (firstName == '' && lastName == '' && mobile == '') {
+        alert("Enter all fields");
+        return false;
+    }
+    if (firstName == '') {
+        alert("FirstName can't be blank");
+        firstName.focus();
+        return false;
+    }   
+    if (lastName == '') {
+        alert("LastName can't be blank");
+        lastName.focus();
+        return false;
+    }
+    if (mobile == '') {
+        alert("Enter your mobile number");
+        mobile.focus();
+        return false;
+    }
+    if (!(gender[0].checked) && !(gender[0].checked)) {
+        alert("Please select gender");
+        return false;
+    }
+    var dotNet = document.getElementById("NetCheckBox").checked;
+    var java = document.getElementById("JavaCheckBox").checked;
+    var sdet = document.getElementById("SdetCheckBox.ClientID").checked;
+   
+    if (!(dotNet || java || sdet)) {
+        document.getElementById("NetCheckBox.ClientID").focus();
+        alert("Please select atleast one stream.");
+        return false;
+    }
+
+  
+    
+}
